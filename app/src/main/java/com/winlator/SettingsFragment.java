da3c81fc350c396a7ba3c83bf17c161bfae975f7
hostei <hostei@qq.com>
2026-04-20 21:25:28 +0800

    修复设置页面主题默认值与应用默认主题不一致

--- app/src/main/java/com/winlator/SettingsFragment.java
+++ app/src/main/java/com/winlator/SettingsFragment.java
@@ -119,7 +119,7 @@ public class SettingsFragment extends Fragment {
         loadBox64PresetSpinner(view, sBox64Preset);
 
         final RadioGroup rgAppTheme = view.findViewById(R.id.RGAppTheme);
-        final int oldAppThemeId = preferences.getInt("app_theme", APP_THEME_DARK) == APP_THEME_DARK ? R.id.RBDark : R.id.RBLight;
+        final int oldAppThemeId = preferences.getInt("app_theme", APP_THEME_LIGHT) == APP_THEME_DARK ? R.id.RBDark : R.id.RBLight;
         rgAppTheme.check(oldAppThemeId);
 
         final CheckBox cbMoveCursorToTouchpoint = view.findViewById(R.id.CBMoveCursorToTouchpoint);
